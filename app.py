a4141d2f710384a0fb78276cc65fe155d51c10b9
Ashe Andrews <asheandrews@proton.me>
2024-11-29 20:51:00 -0500

    Developed adding an entry

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import json
 from flask import Flask, redirect, render_template, request
 
-from helper import read_users
+from helper import read_users, read_entries
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def hello():
-    return render_template("home.html", user_list = read_users())
+    return render_template("home.html", user_list = read_users(),
+                            entry_list = read_entries())
 
 @app.route('/add-user', methods = ['POST'])
 def add_user():
@@ -15,11 +16,31 @@ def add_user():
     user_list = read_users()
 
     with open("./data/users.json", 'w') as users_file:
-        # Add user to list
-        user_list.append(name)
+        if name not in user_list:
+            # Add user to list if it's unique
+            user_list.append(name)
 
         # Save list back to JSON
         json.dump({"users": user_list}, users_file)
+
+    return redirect('/')
+
+@app.route('/add-entry', methods = ['POST'])
+def add_entry():
+    # Get name to add and list to add to
+    name = request.form.get('dropdown')
+    event = request.form.get('event')
+    user_list = read_users()
+    entry_list = read_entries()
+
+    with open("./data/entries.json", 'w') as entry_file:
+        # Add entry to list
+        entry = {"name": name, "event": event}
+        if entry not in entry_list:
+            entry_list.append(entry)
+        
+        # Save list back to JSON
+        json.dump({"entries": entry_list}, entry_file)
 
     return redirect('/')
 
--- helper.py
+++ helper.py
@@ -5,3 +5,10 @@ def read_users() -> list[str]:
         user_list: list = json.load(users_file).get("users", [])
 
     return user_list
+
+
+def read_entries() -> list[dict]:
+    with open("./data/entries.json", 'r') as entries_file:
+        entry_list: list = json.load(entries_file).get("entries", [])
+
+    return entry_list
